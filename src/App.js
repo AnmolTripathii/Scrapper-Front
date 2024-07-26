de787fe3864698fbb903b911499e9a708339bec7
unknown <anmoltripathi610@gmail.com>
2024-07-26 22:50:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from './component/SearchBar/SearchBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import GenDisplay from './pages/GenDisplay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/product">
+            <Route path=":GenId" element={<GenDisplay />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
