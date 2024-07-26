325d2bac4663a43990a6693bef16bac45abdfec1
unknown <anmoltripathi610@gmail.com>
2024-07-26 23:20:45 +0530

    first2

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -8,8 +8,12 @@ function SearchBar() {
 
   const SearchUrl = async (e) => {
     e.preventDefault();
-    console.log(inputRef.current.value);
-    let url = inputRef.current.value;
+    const url = inputRef.current.value.trim();
+    if (!url) {
+      setPopupMessage('Please enter a valid URL.');
+      return;
+    }
+
     try {
       const response = await axios.post('https://scrapper-back-pied.vercel.app/api/v1/webs/generate', { url });
       setPopupMessage(response.data.message);
@@ -18,14 +22,13 @@ function SearchBar() {
       setPopupMessage('Error fetching data');
     }
     e.target.reset();
-  }
+  };
 
   useEffect(() => {
     if (popupMessage) {
       const timer = setTimeout(() => {
         setPopupMessage('');
       }, 3000);
-
       return () => clearTimeout(timer);
     }
   }, [popupMessage]);
@@ -38,7 +41,13 @@ function SearchBar() {
           <svg viewBox="0 0 512 512" className="searchIcon">
             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
           </svg>
-          <input type='text' name='url' placeholder='Enter domain name' ref={inputRef} required />
+          <input
+            type='text'
+            name='url'
+            placeholder='Enter domain name'
+            ref={inputRef}
+            required
+          />
         </div>
         <button type='submit' className='Search_btn'>Fetch & Save Details</button>
       </form>
